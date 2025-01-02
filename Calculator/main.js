0ac0985807c61b488d2c363c5fe789d902e054ca
Jyotirmay <jyotirmayzamre@gmail.com>
2025-01-02 12:53:00 +0530

    Created HTML layout for calculator

---
+++ Calculator/main.js
@@ -0,0 +1,21 @@
+let dig1;
+let dig2;
+let oper;
+
+
+function add(dig1, dig2){
+    return dig1 + dig2;
+}
+
+function subtract(dig1, dig2){
+    return dig1 - dig2;
+}
+
+function mult(dig1, dig2){
+    return dig1 * dig2;
+}
+
+function div(dig1, dig2){
+    return dig1 / dig2;
+}
+
