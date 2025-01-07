bdde6e3c989c5dcc4fbb1ef43028a22b61bcf690
Joe <jyotirmayzamre@gmail.com>
2025-01-07 23:39:41 +0530

    Set up files

---
+++ library/main.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function(){
+    let phrase = this.read ? 'read' : 'not read yet'
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${phrase}`
+}
+
+function addBookToLibrary(title, author, pages, read){
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
