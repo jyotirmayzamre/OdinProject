61bf22c6d1fb1f352d740fd79fa4b2e3beab47f3
Joe <jyotirmayzamre@gmail.com>
2025-01-08 21:34:05 +0530

    Set up the dialog box for the form

--- library/main.js
+++ library/main.js
@@ -1,5 +1,8 @@
 const myLibrary = [];
+const addBook = document.getElementById('new-book');
+const dialog = document.querySelector("dialog");
 
+//Book constructor
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -7,13 +10,20 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
+//Book information method
 Book.prototype.info = function(){
     let phrase = this.read ? 'read' : 'not read yet'
     return `${this.title} by ${this.author}, ${this.pages} pages, ${phrase}`
 }
 
+//function for creating + adding books
 function addBookToLibrary(title, author, pages, read){
     let book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
 
+
+addBook.addEventListener('click', function(){
+    dialog.showModal();
+    console.log('hello');
+});
