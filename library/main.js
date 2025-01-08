c9b8162c0470698ced5c8872727012cac95c126f
Joe <jyotirmayzamre@gmail.com>
2025-01-08 22:58:42 +0530

    Created cards for the books

--- library/main.js
+++ library/main.js
@@ -1,19 +1,16 @@
 const myLibrary = [];
 const addBook = document.getElementById('new-book');
 const dialog = document.querySelector("dialog");
+const bookForm = document.querySelector('form');
+const submitButton = document.getElementById('submit');
+const cardContainer = document.getElementById('card-container');
 
 //Book constructor
-function Book(title, author, pages, read){
+function Book(title, author, pages, isRead){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-}
-
-//Book information method
-Book.prototype.info = function(){
-    let phrase = this.read ? 'read' : 'not read yet'
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${phrase}`
+    this.isRead = isRead;
 }
 
 //function for creating + adding books
@@ -25,5 +22,50 @@ function addBookToLibrary(title, author, pages, read){
 
 addBook.addEventListener('click', function(){
     dialog.showModal();
-    console.log('hello');
 });
+
+//function to create a card in the dom
+function createCard(formData){
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('card');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const read = document.createElement('button');
+    const remove = document.createElement('button');
+    remove.textContent = 'Remove';
+    remove.classList.add('btn');
+    read.classList.add('btn');
+
+    title.textContent = formData.get('title');
+    author.textContent = formData.get('author');
+    pages.textContent = formData.get('pages') + ' pages';
+    
+    const status = formData.get('read');
+    if(status === 'on'){
+        read.classList.add('read');
+        read.textContent = 'Read';
+    } else{
+        read.classList.add('nread');
+        read.textContent = 'Not Read';
+    }
+
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(read);
+    bookCard.appendChild(remove);
+
+    cardContainer.appendChild(bookCard);
+
+}
+
+//event listener for submitting the form
+submitButton.addEventListener('click', function(e){
+    e.preventDefault();
+    const formData = new FormData(bookForm);
+    dialog.close();
+    createCard(formData); 
+});
+
+
