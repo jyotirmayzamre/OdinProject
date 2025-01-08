5d5e4c2bbd6654de63062ce5fe32a6738f64a887
Joe <jyotirmayzamre@gmail.com>
2025-01-09 00:10:51 +0530

    Finished all the desired functionality. Final commit.

--- library/main.js
+++ library/main.js
@@ -13,10 +13,16 @@ function Book(title, author, pages, isRead){
     this.isRead = isRead;
 }
 
+
+Book.prototype.changeStatus = function(){
+    this.isRead = !this.isRead;
+}
+
 //function for creating + adding books
-function addBookToLibrary(title, author, pages, read){
-    let book = new Book(title, author, pages, read);
+function addBookToLibrary(formData){
+    const book = new Book(formData.get('title'), formData.get('author'), formData.get('pages'), formData.get('read') === 'on' ? true : false);
     myLibrary.push(book);
+    return book;
 }
 
 
@@ -24,25 +30,49 @@ addBook.addEventListener('click', function(){
     dialog.showModal();
 });
 
+//function for changing the read status
+function changeStatus(e, book){
+    const elem = e.target;
+    const currStatus = book.isRead;
+    book.changeStatus();
+    if(currStatus === true){
+        elem.classList.remove('read');
+        elem.classList.add('nread');
+        elem.textContent = 'Not Read';
+    } else{
+        elem.classList.remove('nread');
+        elem.classList.add('read');
+        elem.textContent = 'Read';
+    }
+}
+
+
 //function to create a card in the dom
-function createCard(formData){
+function createCard(book){
     const bookCard = document.createElement('div');
     bookCard.classList.add('card');
+
+    //title
     const title = document.createElement('p');
+    title.textContent = book.title;
+    bookCard.appendChild(title);
+
+    //author
     const author = document.createElement('p');
+    author.textContent = book.author;
+    bookCard.appendChild(author);
+
+    //pages
     const pages = document.createElement('p');
+    pages.textContent = book.pages + ' pages';
+    bookCard.appendChild(pages);
+
+    //read status button
     const read = document.createElement('button');
-    const remove = document.createElement('button');
-    remove.textContent = 'Remove';
-    remove.classList.add('btn');
     read.classList.add('btn');
 
-    title.textContent = formData.get('title');
-    author.textContent = formData.get('author');
-    pages.textContent = formData.get('pages') + ' pages';
-    
-    const status = formData.get('read');
-    if(status === 'on'){
+    const status = book.isRead;
+    if(status){
         read.classList.add('read');
         read.textContent = 'Read';
     } else{
@@ -50,22 +80,36 @@ function createCard(formData){
         read.textContent = 'Not Read';
     }
 
-    bookCard.appendChild(title);
-    bookCard.appendChild(author);
-    bookCard.appendChild(pages);
+    read.addEventListener('click', (e) => changeStatus(e, book));
+
+
     bookCard.appendChild(read);
+
+    //remove button
+    const remove = document.createElement('button');
+    remove.textContent = 'Remove';
+    remove.classList.add('btn');
+
+    remove.addEventListener('click', function(){
+        myLibrary.splice(myLibrary.indexOf(book), 1);
+        cardContainer.removeChild(bookCard);
+    })
+
     bookCard.appendChild(remove);
+    
+
 
     cardContainer.appendChild(bookCard);
-
 }
+
 
 //event listener for submitting the form
 submitButton.addEventListener('click', function(e){
     e.preventDefault();
     const formData = new FormData(bookForm);
     dialog.close();
-    createCard(formData); 
+    bookForm.reset();
+    createCard(addBookToLibrary(formData)); 
 });
 
 
