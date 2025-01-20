f2e614d7e05b062a149d05dd3dfea59a7dad7a58
Joe <jyotirmayzamre@gmail.com>
2025-01-20 11:39:21 +0530

    Finished page layout

--- todo/webpack.config.js
+++ todo/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
-        cleanr: true,
+        clean: true,
     },
     devtool: "eval-source-map",
     devServer: {
