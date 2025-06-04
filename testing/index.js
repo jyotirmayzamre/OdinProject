30ae48c1f2b9ba420e5a84a6967039ef6b50ea53
Joe <jyotirmayzamre@gmail.com>
2025-06-04 18:23:47 +0530

    Finished testing assignment

--- testing/index.js
+++ testing/index.js
@@ -1,3 +1,88 @@
 function capital(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+function reverse(string){
+    let reversedStr = '';
+    for (let i = string.length - 1; i >= 0; i--){
+        reversedStr += string[i];
+    }
+    return reversedStr;
+}
+
+class Calculator {
+    add(x, y){
+        return x + y;
+    }
+
+    subtract(x, y){
+        return x - y;
+    }
+
+    multiply(x, y){
+        return x*y;
+    }
+
+    divide(x, y){
+        return x / y;
+    }
+}
+
+function caesarCipher(str, num){
+    let cipher = '';
+
+    for(let char of str) {
+        let isAlphabet = /[a-z]/i.test(char);
+        let isUpper = /[A-Z]/.test(char);
+
+        if(isAlphabet){
+            let charCode = char.charCodeAt();
+
+            if(isUpper){
+                charCode -= 65;
+                charCode += num;
+                charCode %= 26;
+                charCode += 65;
+            } else {
+                charCode -= 97;
+                charCode += num;
+                charCode %= 26;
+                charCode += 97;
+                
+            }
+            cipher += String.fromCharCode(charCode);
+        } else {
+            cipher += char;
+        }
+    }
+    return cipher;
+}
+
+function analyzeArray(arr){
+    let min = arr[0];
+    let max = arr[0];
+    let length = 0;
+    let sum = 0;
+    for(let num of arr){
+        length += 1;
+        sum += num;
+        if(num < min) min = num;
+        if(num > max) max = num;
+    }
+    return {
+        average: sum / length,
+        min: min,
+        max: max,
+        length: length
+    }
+}
+
+
+module.exports = {
+    capital: capital,
+    reverse: reverse,
+    calculator: new Calculator(),
+    caesarCipher: caesarCipher,
+    analyzeArray: analyzeArray,
+
+}
--- testing/main.test.js
+++ testing/main.test.js
@@ -1,5 +1,43 @@
-const capital = require('./index');
+const { capital, reverse, calculator, caesarCipher, analyzeArray } = require('./index');
 
 test('Checks for capital letter', () => {
-    expect(capital('hello').toMatch('^[A-Z].*$'))
+    expect(capital('hello')).toMatch(/^[A-Z].*$/);
+})
+
+test('Checks for string reversal', () => {
+    expect(reverse('hello')).toBe('olleh');
+})
+
+test('Addition', ()=>{
+    expect(calculator.add(3, 4)).toBe(7);
+})
+
+test('Subtraction', () => {
+    expect(calculator.subtract(5, 2)).toBe(3);
+})
+
+test('Multiply', () => {
+    expect(calculator.multiply(4, 5)).toBe(20);
+})
+
+test('Divide', () => {
+    expect(calculator.divide(6, 3)).toBe(2);
+})
+
+
+test('Array func', () => {
+    expect(analyzeArray([1,8,3,4,2,6])).toEqual({ average: 4, min: 1, max: 8, length: 6});
+})
+
+test('cipher', () => {
+    expect(caesarCipher('xyz', 3)).toBe('abc');
+})
+
+test('cipher2', () => {
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+})
+
+
+test('cipher3', () => {
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
 })
