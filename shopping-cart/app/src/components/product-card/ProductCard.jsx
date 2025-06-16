d668c784ce2863c093c60f798ef0857557e75d26
Joe <jyotirmayzamre@gmail.com>
2025-06-16 11:34:44 +0530

    Set up the initial component for the product card

---
+++ shopping-cart/app/src/components/product-card/ProductCard.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./ProductCard.css";
+
+function ProductCard({ product, addToCart }){
+    const [quantity, setQuantity] = useState(1);
+
+
+    function increment(){
+        setQuantity(prev => prev + 1);
+    }
+
+    function decrement(){
+        setQuantity(prev => Math.min(prev - 1, 1));
+    }
+
+    function set(e){
+        const val = e.target.value;
+        if(val < quantity){
+            setQuantity(Math.min(val, 1));
+        } else{
+            setQuantity(val);
+        }
+    }
+    
+
+    return (
+        <div className="productCard">
+            <div className="img-container">
+                <img src={product.Url} alt={product.Name} loading="lazy"></img>
+            </div>
+            <div className="info-container">
+                <h3>{product.Name}</h3>
+                <p>{product.Price}</p>
+                <div className="quantityControls">
+                    <button className='btn' onClick={decrement}>-</button>
+                    <input type="number" value={quantity} min="1" aria-label="product-quantity" onChange={set}></input>
+                    <button className="btn" onClick={increment}>+</button>
+                </div>
+                <button className="btn" onClick={addToCart}>Add to Cart</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProductCard;
