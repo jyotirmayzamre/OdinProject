dd9eacd1b395dcfba639d3482ec2e9ec3df9fe0d
Joe <jyotirmayzamre@gmail.com>
2025-07-10 12:22:32 +0530

    Updated deleteFiles utility. Todo: add share folder functionality and completely refactor the API to make it cleaner

--- file-uploader/public/utilities/deleteFiles.js
+++ file-uploader/public/utilities/deleteFiles.js
@@ -1,17 +1,22 @@
 const fs = require('fs/promises');
 const path = require('path');
 
+/*
+Things to note: no need to return await fs.link because each async function returns a promise once fully resolved
+If a single delete fails, the async function promise is still resolved because error is caught and logged
+This ensures that other deletes are not affected
+*/
 async function deleteFiles(locs){
-    await Promise.all(
-        locs.map(async (temp) => {
-            try {
-                const newPath = path.join(__dirname, '../..', temp.location);
-                await fs.unlink(newPath);
-            } catch(err){
-                console.error(err);
-            }
-        })
-    )
+    const promises = locs.map(async (temp) => {
+        try {
+            const newPath = path.join(__dirname, '../..', temp.location);
+            await fs.unlink(newPath);
+        } catch(e){
+            console.error(e);
+        }
+        
+    })
+    await Promise.all(promises);
 }
 
 module.exports = deleteFiles;
